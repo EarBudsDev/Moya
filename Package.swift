c49d4817c1d90386e7940f35c4a5bd90e36ed5a1
Samuel Goodwin <samuel@roundwallsoftware.com>
2022-10-27 09:45:02 +0200

    This removes all the reactive/rx stuff.
    We don't need it and neither does Moya, so here's what happens if we
    remove it and also get rid of mentions of Carthage and such. Moya was an
    old enough project, it was including it's dependencies in old ways so I
    also removed that too.

--- Package.swift
+++ Package.swift
@@ -2,16 +2,6 @@
 
 import PackageDescription
 
-let rocketIfNeeded: [Package.Dependency]
-
-#if os(OSX) || os(Linux)
-rocketIfNeeded = [
-    .package(url: "https://github.com/shibapm/Rocket", .upToNextMajor(from: "1.2.0")) // dev
-]
-#else
-rocketIfNeeded = []
-#endif
-
 let package = Package(
     name: "Moya",
     platforms: [
@@ -21,10 +11,8 @@ let package = Package(
         .watchOS(.v3)
     ],
     products: [
-        .library(name: "Moya", targets: ["Moya"]),
-        .library(name: "CombineMoya", targets: ["CombineMoya"]),
-        .library(name: "ReactiveMoya", targets: ["ReactiveMoya"]),
-        .library(name: "RxMoya", targets: ["RxMoya"])
+    .library(name: "Moya", targets: ["Moya"]),
+    .library(name: "CombineMoya", targets: ["CombineMoya"]),
     ],
     dependencies: [
         .package(url: "https://github.com/Alamofire/Alamofire.git", .upToNextMajor(from: "5.0.0")),
@@ -33,7 +21,7 @@ let package = Package(
         .package(url: "https://github.com/Quick/Quick.git", .upToNextMajor(from: "4.0.0")), // dev
         .package(url: "https://github.com/Quick/Nimble.git", .upToNextMajor(from: "9.0.0")), // dev
         .package(url: "https://github.com/AliSoftware/OHHTTPStubs.git", .upToNextMajor(from: "9.0.0")) // dev
-    ] + rocketIfNeeded,
+    ],
     targets: [
         .target(
             name: "Moya",
@@ -43,55 +31,23 @@ let package = Package(
             exclude: [
                 "Supporting Files/Info.plist"
             ]
-        ),
+            ),
         .target(
             name: "CombineMoya",
             dependencies: [
                 "Moya"
             ]
-        ),
-        .target(
-            name: "ReactiveMoya",
-            dependencies: [
-                "Moya",
-                .product(name: "ReactiveSwift", package: "ReactiveSwift")
-            ]
-        ),
-        .target(
-            name: "RxMoya",
-            dependencies: [
-                "Moya",
-                .product(name: "RxSwift", package: "RxSwift")
-            ]
-        ),
+            ),
         .testTarget( // dev
-            name: "MoyaTests",  // dev
-            dependencies: [ // dev
-                "Moya", // dev
-                "CombineMoya", // dev
-                "ReactiveMoya", // dev
-                "RxMoya", // dev
-                .product(name: "Quick", package: "Quick"), // dev
-                .product(name: "Nimble", package: "Nimble"), // dev
-                .product(name: "OHHTTPStubsSwift", package: "OHHTTPStubs") // dev
-            ] // dev
-        ) // dev
+                    name: "MoyaTests",  // dev
+                    dependencies: [ // dev
+                        "Moya", // dev
+                        "CombineMoya", // dev
+                        .product(name: "Quick", package: "Quick"), // dev
+                        .product(name: "Nimble", package: "Nimble"), // dev
+                        .product(name: "OHHTTPStubsSwift", package: "OHHTTPStubs") // dev
+                    ] // dev
+                    ) // dev
     ]
 )
 
-#if canImport(PackageConfig)
-import PackageConfig
-
-let config = PackageConfiguration([
-    "rocket": [
-        "before": [
-            "scripts/update_changelog.sh",
-            "scripts/update_podspec.sh"
-        ],
-        "after": [
-            "rake create_release\\[\"$VERSION\"\\]",
-            "scripts/update_docs_website.sh"
-        ]
-    ]
-]).write()
-#endif
